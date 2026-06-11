899e873f2d26e630dda8dd0d0c96199fc54f1401
Fahad Tahir <fahadtahir94@gmail.com>
2026-06-11 11:52:54 +0300

    MS Clarity

--- public/js/auth.js
+++ public/js/auth.js
@@ -35,3 +35,11 @@ function esc(s) {
     .replace(/</g, '&lt;')
     .replace(/>/g, '&gt;');
 }
+
+<script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "x5a9kjj4e2");
+</script>
